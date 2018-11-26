ce635040ac5cc2e4e36cabc79613e6caa70ca2df
Brent Lee <brenthlee@gmail.com>
2018-11-25 20:52:03 -0800

    set up the initial files with some input and output statements and comments

--- DiGraph.java
+++ DiGraph.java
@@ -5,9 +5,58 @@
  * Project 5
  */
 
+import java.util.*;
+
 public class DiGraph {
 
-    public static void main(String[] args) {
-        
+    private LinkedList<Integer>[] arr;
+
+    /* DiGraph constructor initialized */
+    public DiGraph(int N) {
+        arr = (LinkedList<Integer>[])new LinkedList[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = new LinkedList<Integer>();
+        }
+    }
+
+    /*
+     * two parameters identify vertices representing the edge that needs
+     * to be added to the graph
+     * // "to" vertex is added as "from" vertex's neighbor
+     */
+    public void addEdge(int from, int to) {
+        // the edge should NOT be added if it already exists (check before adding)
+        // vertex-numbers are given in natural numbering (start with 1)
+        //      so turn them into Java-indexing to reflect correct connection
+        //      NO NEED FOR VALIDITY CHECK
+    }
+
+    /* 
+     * two parameters identify vertices representing the edge that needs
+     * to be deleted from the graph
+     * // "to" vertex is removed as "from" vertex's neighbor
+     */
+    public void deleteEdge(int from, int to) {
+        // nothing is done if such edge doesn't exist
+        // NO ERROR MESSAGE OR EXCEPTION
+    }
+
+    /* computes and returns the number of edges in the graph */
+    public int edgeCount() {
+    }
+    
+    /* returns the number of vertices in the graph (the array's length) */
+    public int vertexCount() {
+        return arr.length;
+    }
+
+    /*
+     * outputs the graph in the following format:
+     * for each vertex i (from 0 to n-1) outputs a line:
+     * i is connected to: x1,...,xk // how it should print
+     * where x1,...,xk are vertices that are adjacent to i
+     * // number of vertices should be natural indexing
+     */
+    public void print() {
     }
 }
--- DiGraphTest.java
+++ DiGraphTest.java
@@ -5,9 +5,41 @@
  * Project 5
  */
 
+import java.util.*;
+
 public class DiGraphTest {
 
     public static void main(String[] args) {
-        
+        int n; //number of vertices
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of vertices: ");
+        n = scanner.nextInt();
+        DiGraph graph = new DiGraph(n);
+        char choice;
+        System.out.println("- add edge (enter a)");
+        System.out.println("- deleted edge (enter d)");
+        System.out.println("- edge count (enter e)");
+        System.out.println("- vertex count (enter v)");
+        System.out.println("- print graph (enter p)");
+        System.out.println("- Quit (enter q)");
+        do {
+            System.out.println("Choose one of the following operations:");
+            choice = scanner.next().charAt(0); // reads character
+            switch (choice) {
+                case 'a': // requires input
+                    break;
+                case 'd': // requires input
+                    break;
+                case 'e':
+                    break;
+                case 'v':
+                    break;
+                case 'p':
+                    break;
+                default:
+                    break;
+        } while (choice != 'q');
+
     }
 }
